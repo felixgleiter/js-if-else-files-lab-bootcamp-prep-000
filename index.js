7eb7983b88725f0647e62583809701b82cbdf6a6
Felix Godfree <felixfgodfree@gmail.com>
2018-08-26 13:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var height = "74"
 
 if (name === "Susan") {
     console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
+} 
+
+if (height === "70") {
+  console.log("The height is correct")
 }
 
 // Don't worry about this
